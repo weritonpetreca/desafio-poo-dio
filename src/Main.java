a59ed6dda0bf1297547ec76eea77f8b2343d1c59
Weriton Luis Petreca <eulcf@hotmail.com>
2025-07-06 14:13:34 -0300

    feat: Curso and Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
+import com.petreca.desafio.dominio.Curso;
+import com.petreca.desafio.dominio.Mentoria;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Aprenda os conceitos básicos de Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de JavaScript");
+        curso2.setDescricao("Aprenda os conceitos básicos de JavaScript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Sessão de perguntas e respostas sobre Java");
+        mentoria.setData(java.time.LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
 
     }
 }
